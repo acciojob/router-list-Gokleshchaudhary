321c2e1ad74ea837c9830773bbed6a58abf5e238
goklesh chaudhary <123gopalchaudhary123@gmail.com>
2025-08-01 13:49:33 +0530

    Update ItemDetail.js

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,1 +1,20 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import items from "./data";
+
+function ItemDetail() {
+  const { id } = useParams();
+  const item = items.find((item) => item.id === id);
+
+  if (!item) return <p>Item not found</p>;
+
+  return (
+    <div>
+      <h1>{item.name}</h1>
+      <p>{item.description}</p>
+    </div>
+  );
+}
+
+export default ItemDetail;
 
